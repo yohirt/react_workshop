563317e76562775e71cbe4332c17d2431f2070e7
Kamil <fenek@autograf.pl>
2019-03-30 21:04:02 +0100

    2

---
+++ src/components/StarWarsApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function  StarWarsApp () {
+    return <div className="d-flex flex-column align-items-center">
+        <h1>Star Wars Planets</h1>
+        <div className="spinner-border text-primary"></div>
+    </div>
+  };
+
+export default StarWarsApp;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function  Title () {
-  return <div className="d-flex flex-column align-items-center">
-      <h1>Star Wars Planets</h1>
-      <div className="spinner-border text-primary"></div>
-  </div>
-};
+import StarWarsApp from './components/StarWarsApp';
 
 ReactDOM.render(
-  <Title/>,
+  <StarWarsApp/>,
   document.getElementById('app')
 );
 
