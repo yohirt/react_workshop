6f4a86f414194b779d43bfaaa60a7600d729e485
Kamil <fenek@autograf.pl>
2019-03-30 21:06:11 +0100

    2ex

---
+++ src/components/Loader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Loader () {
+    return <div className="spinner-border text-primary"></div>;
+}
+
+export default Loader;
--- src/components/StarWarsApp.js
+++ src/components/StarWarsApp.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Loader from './Loader';
 
 function  StarWarsApp () {
     return <div className="d-flex flex-column align-items-center">
         <h1>Star Wars Planets</h1>
-        <div className="spinner-border text-primary"></div>
+        <Loader/>
     </div>
   };
 
