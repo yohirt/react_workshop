328e3db0f469c27f5e70fbfc28d8f5e8364c7ecb
Kamil <fenek@autograf.pl>
2019-03-30 21:37:17 +0100

    4

--- src/components/StarWarsApp.js
+++ src/components/StarWarsApp.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import Loader from './Loader';
+import searchPlanets from '../providers/SWAPIFetcher';
 
-function  StarWarsApp () {
-    return <div className="d-flex flex-column align-items-center">
-        <h1>Star Wars Planets</h1>
-        <Loader isLoading={true}/>
-    </div>
-  };
+class  StarWarsApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchLoading: false,
+            planets: []
+        }
+    }
+
+    async componentDidMount() {
+        this.setState({
+            searchLoading: true
+        });
+
+        const planets  = await searchPlanets('a');
+
+        this.setState({
+            searchLoading: false,
+            planets
+        });
+    }
+
+    render() {
+        return <div className="d-flex flex-column align-items-center">
+            <h1>Star Wars Planets</h1>
+            <Loader isLoading={this.state.searchLoading}/>
+            <p>
+                {this.state.planets.length && this.state.planets[0].name}
+            </p>
+        </div>
+    };
+}
 
 export default StarWarsApp;
